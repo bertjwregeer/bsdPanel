789197c489e449b4e5e4f144eb367aad558c5fdd
Bert JW Regeer <bertjw@regeer.org>
2005-01-20 21:33:59 +0000

    Copyright header and info has been added to all source files.
    git-svn-id: http://svn.0x58.com/repos/bsdpanel/trunk@39 db258d53-41f1-0310-98c4-cc461a13e28c

--- bin/includes/Fork/Fork.cc
+++ bin/includes/Fork/Fork.cc
@@ -1,30 +1,37 @@
+/*
+ * Copyright 2005 X-Istence.com. All rights  reserved.
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and  documentation are
+ * those of the authors and should not be  interpreted as representing official
+ * policies, either expressed  or implied, of X-Istence.com or the bsdPanel
+ * project.
+ *
+ * $Id$
+ */
+
 #include "Fork.h"
-
-#ifndef HAVEOWNHANDLERS
-namespace sighandlers {
-        static void sig_chld(int signo) 
-        {
-                pid_t mypid;
-                int status, child_val;
-        
-                /* Wait for any child without blocking */
-                if ((mypid = waitpid(-1, &status, WNOHANG)) < 0) {
-                        return;
-                }
-        
-                /*
-                * We now have the info in 'status' and can manipulate it using
-                * the macros in wait.h.
-                */
-                if (WIFEXITED(status))                /* did child exit normally? */
-                {
-                        child_val = WEXITSTATUS(status); /* get child's exit status */
-                        std::cerr << "Child: " << mypid << " has exited with status: " << child_val << "\n";
-                }
-        }
-}
-
-#endif
 
 int bsdPanel::Fork::doFork () {
 
