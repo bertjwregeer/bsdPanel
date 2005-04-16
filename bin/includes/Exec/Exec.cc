828355e3f1cf9fc24d8037db37e43c206dd0a030
Bert JW Regeer <bertjw@regeer.org>
2005-04-16 22:17:29 +0000

    Added the Exec files
    git-svn-id: http://svn.0x58.com/repos/bsdpanel/trunk@77 db258d53-41f1-0310-98c4-cc461a13e28c

---
+++ bin/includes/Exec/Exec.cc
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2005 X-Istence.com. All rights  reserved.
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and  documentation are
+ * those of the authors and should not be  interpreted as representing official
+ * policies, either expressed  or implied, of X-Istence.com or the bsdPanel
+ * project.
+ *
+ * $Id$
+ */
+ 
+#include <Exec.h>
+ 
+bsdPanel::Exec::Exec(std::string& _prog, std::vector<std::string>& _args) : prog(_prog), args(_args) {
+        if (args.size() > MAXARG)
+                throw bsdPanel::Exec::TooManyArgs();
+}
+
+bsdPanel::Exec::Exec(std::string& _prog, std::vector<std::string>& _args, std::vector<std::string>& _env) : prog(_prog), args(_args), env(_env) {
+        if (args.size() > MAXARG)
+                throw bsdPanel::Exec::TooManyArgs();
+        if (env.size() > MAXARG)
+                throw bsdPanel::Exec::TooManyEnv();
+}
+
+int bsdPanel::Exec::doExec() {
+        char *argv[MAXARG];
+        char *envi[MAXARG];
+        
+        if (args.size() > 0) {
+                int i = 0;
+                for (std::vector<std::string>::iterator a = args.begin(), b = args.end(); 
+                        a!=b; ++a) {
+                        argv[i] = new char[a->length() + 1]; 
+                        strcpy(argv[i], a->c_str());
+                        i++;
+                }
+        }
+        
+        if (env.size() > 0) {
+                int i = 0;
+                for (std::vector<std::string>::iterator a = env.begin(), b = env.end(); 
+                        a!=b; ++a) {
+                        envi[i] = new char[a->length() + 1]; 
+                        strcpy(envi[i], a->c_str());
+                        i++;
+                }
+        }
+        
+        if ( (args.size() > 0) && (env.size() == 0) ) {
+                switch(doPipe()) {
+                        case 0:
+                                execv(prog.c_str(), argv);
+                                bsdPanel::exit_program(-1);
+                                break;
+                        case 1:
+                                return 1;
+                        default:
+                                bsdPanel::exit_program();
+                                // If we get here there is a more serious problem :P
+                }
+        }
+        else if ( (args.size() > 0) && (env.size() > 0) ) {
+                switch(doPipe()) {
+                        case 0:
+                                execve(prog.c_str(), argv, envi);
+                                bsdPanel::exit_program(-1);
+                                break;
+                        case 1:
+                                return 1;
+                        default:
+                                bsdPanel::exit_program();
+                                // If we get here there are more serious problems :P
+                }
+        }
+        else {
+                throw bsdPanel::Exec::NoArgs();
+        }
+// If we get here, something is wrong :P
+return 0;
+}                   
