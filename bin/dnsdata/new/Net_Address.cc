543f45137b92f3292ec8072e9ddcc6fcbc6fa570
Geoff Garside <geoff@geoffgarside.co.uk>
2005-12-29 23:05:31 +0000

    Now compiles
    git-svn-id: http://svn.0x58.com/repos/bsdpanel/trunk@135 db258d53-41f1-0310-98c4-cc461a13e28c

--- bin/dnsdata/new/Net_Address.cc
+++ bin/dnsdata/new/Net_Address.cc
@@ -66,7 +66,7 @@ namespace bsdPanel
             // break string into pieces by the delimiter
             std::vector<std::string> * string_part = new std::vector<std::string>;
             std::string single_part;
-            for (int i = 0; i < string.size(); i++)
+            for (std::size_t i = 0; i < string.size(); i++)
             {
                 if (delim == string[i])
                 {
