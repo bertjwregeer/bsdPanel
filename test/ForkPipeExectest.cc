23d7e3d680d7601ac37d0b68d7495b93b7dcb4ce
Bert JW Regeer <bertjw@regeer.org>
2006-08-13 11:34:34 +0000

    Updated tests for the classes Fork Pipe and Exec
    git-svn-id: http://svn.0x58.com/repos/bsdpanel/trunk@199 db258d53-41f1-0310-98c4-cc461a13e28c

--- test/ForkPipeExectest.cc
+++ test/ForkPipeExectest.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <0x58-unix/Fork.h>
+#include <0x58-unix/Pipe.h>
 #include <0x58-unix/Exec.h>
 
 int main() {
@@ -9,16 +11,37 @@ int main() {
         
         blah.push_back(std::string("/usr/bin/find"));
         blah.push_back(std::string("."));
+
+#ifdef TESTING       
+        x58unix::Pipe::pair_pipes * pipes = new x58unix::Pipe::pair_pipes();
         
-        x58unix::Exec *MyExec = new x58unix::Exec(blah);
-        MyExec->doExec();
+        x58unix::Exec *MyExec = new x58unix::Exec(new x58unix::Pipe(new x58unix::Fork(), pipes), blah);
+
+        MyExec->execute();
                 
         std::string mystring;
         
-        while (MyExec->read(mystring) > 0)
+        while ( std::getline(pipes->first, mystring) ) {
                 std::cout << mystring << std::endl;
+        }
+        
+        std::cout << std::flush;
+        
+        delete MyExec;
+        
+        MyExec = new x58unix::Exec(new x58unix::Fork(), blah);
 	
-	delete MyExec;        
-
+	MyExec->execute();
+	
+	delete MyExec;
+	
+	sleep(5);
+#endif        
+        x58unix::Exec *MyExec = new x58unix::Exec(new x58unix::Noop(x58unix::Noop::Child), blah);
+	
+	MyExec->execute();
+	
+	delete MyExec;
+        
 return 0;
 }
